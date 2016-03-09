692067ad4a259d8b99edd8e1926c4ed83f39fc58
Library Admin <verneri.hirvonen@edu.jns.fi>
2016-03-09 03:24:54 +0200

    initialization

---
+++ JoystickMonitor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.DirectInput;
+using System.Threading;
+using System.Windows;
+
+namespace JoystrickControlDemo
+{
+    class JoystickMonitor
+    {
+        private String _joystickName;
+
+        // initialize object variables
+        public JoystickMonitor(string joystickName)
+        {
+            _joystickName = joystickName;
+        }
+
+        // Poll joystick state
+        public void PollJoystick(IProgress<JoystickUpdate> progress, CancellationToken cancellationToken)
+        {
+            var directInput = new DirectInput();
+            int devicesFound = 0;
+            DeviceInstance device = null;
+
+            foreach (var d in directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AttachedOnly))
+            {
+                String trimmedProductName = d.ProductName.Substring(0, _joystickName.Length);
+
+                if (devicesFound > 0)
+                {
+                    throw new Exception("More than one Joystick present, only one allowed!");
+                }
+
+                if (String.Compare(trimmedProductName, _joystickName) == 0 )
+                {
+                    devicesFound++;
+                    device = d;
+                }
+            }
+
+            // verify that an input device was found
+            if (device == null)
+            {
+                throw new Exception(String.Format("No joystick with name \"{0}\" found!", _joystickName));
+            }
+
+            var joystick = new Joystick(directInput, device.InstanceGuid);
+
+            joystick.Properties.BufferSize = 128;
+            joystick.Acquire();
+
+            // Loop until a CancellationRequest is made
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                joystick.Poll();
+                JoystickUpdate[] states = joystick.GetBufferedData();
+
+                foreach (var state in states)
+                {
+                    progress.Report(state);
+                }
+            }
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿//TODO:
+//  - reset screen when disconnecting from joystick
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SharpDX.DirectInput;
+using System.Threading;
+
+namespace JoystrickControlDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private const int XAxisMax = 65535;
+        private const int YAxisMax = 65535;
+        private const int ZAxisMax = 65535;
+
+
+        string DefaultJoystickNameTextBoxMessage = "Logitech Extreme 3D";
+
+        JoystickMonitor joystickMonitor;
+        CancellationTokenSource joystickMonitorCancellationSource;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            JoystickNameTextBox.Text = DefaultJoystickNameTextBoxMessage;
+
+
+
+        }
+
+        private void PreviewGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PreviewGridBorder.BorderBrush = new SolidColorBrush(Colors.Red);
+        }
+
+        private void PreviewGrid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            PreviewGridBorder.BorderBrush = new SolidColorBrush(Colors.Black);
+        }
+
+        private void JoystickNameTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (JoystickNameTextBox.Text == DefaultJoystickNameTextBoxMessage)
+            {
+                JoystickNameTextBox.Text = "";
+            }
+        }
+
+        private void JoystickNameTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (JoystickNameTextBox.Text == "")
+            JoystickNameTextBox.Text = DefaultJoystickNameTextBoxMessage;
+        }
+
+        private async void JoystickConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // initialize progress bar colours
+                XProgressBar.Foreground = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+                YProgressBar.Foreground = new SolidColorBrush(Color.FromArgb(255, 0, 255, 0));
+                ZProgressBar.Foreground = new SolidColorBrush(Color.FromArgb(255, 29, 101, 255));
+
+                JoystickConnectButton.IsEnabled = false;
+                JoystickDisconnectButton.IsEnabled = true;
+                joystickMonitor = new JoystickMonitor(JoystickNameTextBox.Text.Trim());
+
+                joystickMonitorCancellationSource = new CancellationTokenSource();
+                var progress = new Progress<JoystickUpdate>(s => ProcessJoystickUpdate(s));
+                MainStatusBarMessage.Text = String.Format("Successfully connected to joystick: {0}", JoystickNameTextBox.Text.Trim());
+                await Task.Run(() => joystickMonitor.PollJoystick(progress, joystickMonitorCancellationSource.Token), joystickMonitorCancellationSource.Token);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error! :( | "+ex.Message);
+            }
+        }
+
+        private void ProcessJoystickUpdate(JoystickUpdate state)
+        {
+            if (state.Offset == JoystickOffset.X)
+            {
+                XProgressBar.Value = calculateXAxisPercentage(state.Value);
+            }
+
+            if (state.Offset == JoystickOffset.Y)
+            {
+                YProgressBar.Value = calculateYAxisPercentage(state.Value);
+            }
+
+            if (state.Offset == JoystickOffset.RotationZ)
+            {
+                ZProgressBar.Value = calculateZAxisPercentage(state.Value);
+            }
+        }
+
+
+        private void JoystickDisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            joystickMonitorCancellationSource.Cancel();
+            JoystickConnectButton.IsEnabled = true;
+            JoystickDisconnectButton.IsEnabled = false;
+
+            // dullify joystick progress bars
+            XProgressBar.Foreground = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+            YProgressBar.Foreground = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+            ZProgressBar.Foreground = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+
+            // Output StatusBar message
+            MainStatusBarMessage.Text = String.Format("Successfully Disconnected from joystick: {0}", JoystickNameTextBox.Text.Trim());
+
+        }
+
+        private int calculateXAxisPercentage(int num)
+        {
+            return (int)((double)num / XAxisMax * 100);
+        }
+
+        private int calculateYAxisPercentage(int num)
+        {
+            return (int)((double)num / YAxisMax * 100);
+        }
+
+        private int calculateZAxisPercentage(int num)
+        {
+            return (int)((double)num / ZAxisMax * 100);
+        }
+    }    
+}
